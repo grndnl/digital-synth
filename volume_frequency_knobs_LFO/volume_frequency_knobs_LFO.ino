8f15b272b87c0c67dd6b0087bf54eeba2dfec7a3
Daniele Grandi <daniele.grandi@autodesk.com>
2020-09-12 21:57:33 -0700

    clock is working! on LED

--- volume_frequency_knobs_LFO/volume_frequency_knobs_LFO.ino
+++ volume_frequency_knobs_LFO/volume_frequency_knobs_LFO.ino
@@ -1,4 +1,4 @@
-// TODO: 
+// TODO:
 // - implement v/oct on the VCO
 // - knobs: root(the frequency becomes the root), octave, scale, range, clock
 // - button: random
@@ -14,25 +14,28 @@
 #include <IntMap.h>
 #include <AutoMap.h>
 #include <LowPassFilter.h>
+#include <Metronome.h>
+#include <EventDelay.h>
+#include <mozzi_rand.h>
 
 #define CONTROL_RATE 128
 
 //MIXER
-const char LEVEL_PIN = 5; 
+const char LEVEL_PIN = 5;
 byte volume;
 
 //VCO
-const char VCO_FREQ_PIN = 4; 
+const char VCO_FREQ_PIN = 4;
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 float VCO_fm_cv;
 const char VCO_fm_cv_pin = 2;
 IntMap VCO_fm_cv_map(0, 1023, 0, 1000);
-const char VCO_fm_pin = 3; 
+const char VCO_fm_pin = 3;
 const char VCO_waveform_pin = 1;
 byte VCO_waveform;
 const int quantize_pin = 7;
 bool quantize;
-IntMap tone_index_map(0, 1023, 0, 59);
+IntMap tone_index_map(0, 1023, 0, 60);
 
 
 
@@ -48,78 +51,123 @@ AutoMap wavetable_choice_map(0, 1023, 0, 3);
 LowPassFilter LPF;
 uint8_t resonance = 255;
 
+// clock
+const byte clock_pin = 6;
+const int clock_LED_pin = LED_BUILTIN;
+IntMap clock_map(0, 1023, 40, 1000);
+int clock_BPM;
+int clock_milli;
+Metronome kMetro;
+EventDelay kClockLED;
+
 
 void set_LFO_wavetable(byte n) {
-  if (n==0) {kLFO.setTable(SIN2048_DATA);}
-  else if (n==1) {kLFO.setTable(TRIANGLE2048_DATA);}
-  else if (n==2) {kLFO.setTable(SAW2048_DATA);}
-  else {kLFO.setTable(SQUARE_NO_ALIAS_2048_DATA);}
+  if (n == 0) {
+    kLFO.setTable(SIN2048_DATA);
+  }
+  else if (n == 1) {
+    kLFO.setTable(TRIANGLE2048_DATA);
+  }
+  else if (n == 2) {
+    kLFO.setTable(SAW2048_DATA);
+  }
+  else {
+    kLFO.setTable(SQUARE_NO_ALIAS_2048_DATA);
+  }
 }
 
 void set_VCO_wavetable(byte n) {
-  if (n==0) {aSin.setTable(SIN2048_DATA);}
-  else if (n==1) {aSin.setTable(TRIANGLE2048_DATA);}
-  else if (n==2) {aSin.setTable(SAW2048_DATA);}
-  else {aSin.setTable(SQUARE_NO_ALIAS_2048_DATA);}
+  if (n == 0) {
+    aSin.setTable(SIN2048_DATA);
+  }
+  else if (n == 1) {
+    aSin.setTable(TRIANGLE2048_DATA);
+  }
+  else if (n == 2) {
+    aSin.setTable(SAW2048_DATA);
+  }
+  else {
+    aSin.setTable(SQUARE_NO_ALIAS_2048_DATA);
+  }
 }
 
 //Quantized frequencies array from A1 to A7
-float note_freq[60] = {55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 
-                       110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 
-                       220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 
-                       440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 
-                       880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22};
+float note_freq[61] = {0, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83,
+                       110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65,
+                       220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30,
+                       440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61,
+                       880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22
+                      };
 
 
 
-void setup(){
+void setup() {
   startMozzi(CONTROL_RATE); // uses the default control rate of 64, defined in mozzi_config.h
   LPF.setResonance(resonance);
   pinMode(quantize_pin, INPUT);
+  pinMode(clock_LED_pin, OUTPUT);
+  kMetro.start(1000);    // set metronome to clock_milli
 }
 
 
-void updateControl(){
-    // set the volume
+void updateControl() {
+  // set the volume
   int level_value = mozziAnalogRead(LEVEL_PIN); // value is 0-1023
   // map it to an 8 bit volume range for efficient calculations in updateAudio
   volume = level_value >> 2;  // 10 bits (0->1023) shifted right by 2 bits to give 8 bits (0->255)
 
   // set the LFO
-  float LFO_freq_value = LFO_map(mozziAnalogRead(VCO_fm_pin))/100.0;
+  float LFO_freq_value = LFO_map(mozziAnalogRead(VCO_fm_pin)) / 100.0;
   // read the VCO FM CV pot
-  VCO_fm_cv = VCO_fm_cv_map(mozziAnalogRead(VCO_fm_cv_pin))/100.0;
+  VCO_fm_cv = VCO_fm_cv_map(mozziAnalogRead(VCO_fm_cv_pin)) / 100.0;
   //set the frequency kLFO
   kLFO.setFreq(LFO_freq_value);
   waveform = wavetable_choice_map(mozziAnalogRead(LFO_waveform_pin));
   set_LFO_wavetable(waveform);
   float vibrato = VCO_fm_cv * kLFO.next();
-  
+
   // set the VCO
   int VCO_frequency = mozziAnalogRead(VCO_FREQ_PIN); // value is 0-1023
   quantize = digitalRead(quantize_pin);
-  if (quantize) { aSin.setFreq(note_freq[tone_index_map(VCO_frequency+ vibrato)] ); }
-  else { aSin.setFreq(VCO_frequency + vibrato); }
+  if (quantize) {
+    aSin.setFreq(note_freq[tone_index_map(VCO_frequency + vibrato)] );
+  }
+  else {
+    aSin.setFreq(VCO_frequency + vibrato);
+  }
   VCO_waveform = wavetable_choice_map(mozziAnalogRead(VCO_waveform_pin));
   set_VCO_wavetable(VCO_waveform);
 
-
   // LPF
-  LPF.setCutoffFreq(250);
+  //  LPF.setCutoffFreq(250);
 
+  // Clock
+  clock_milli= clock_map(mozziAnalogRead(clock_pin));
+  kMetro.set(clock_milli);    // set metronome to clock_milli
+  if (kMetro.ready()) {    // if metro.ready, flash led
+    digitalWrite(clock_LED_pin, HIGH);// turn on led
+    kClockLED.start(10); // eventDelat led.off
   }
+  if (kClockLED.ready()) {
+    digitalWrite(clock_LED_pin, LOW);
+  }
+}
 
 
-int updateAudio(){
+int updateAudio() {
   int audio;
-  if (VCO_waveform !=3 or VCO_waveform != 4) { audio = (int)(aSin.next() * volume) >> 2; } // 8 bits scaled up to 14 bits // https://groups.google.com/g/mozzi-users/c/PxRTWmLySnc
-  else {(int)(aSin.next() * volume) >> 4; }
-//  return LPF.next(audio);
+  if (VCO_waveform != 3 or VCO_waveform != 4) {
+    audio = (int)(aSin.next() * volume) >> 2;  // 8 bits scaled up to 14 bits // https://groups.google.com/g/mozzi-users/c/PxRTWmLySnc
+  }
+  else {
+    (int)(aSin.next() * volume) >> 4;
+  }
+  //  return LPF.next(audio);
   return audio;
 
 }
 
 
-void loop(){
+void loop() {
   audioHook(); // required here
 }
